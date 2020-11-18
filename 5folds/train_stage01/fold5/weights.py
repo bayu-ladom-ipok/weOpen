4b76e0d4db1367180ffb20258375e1b7e19d2dce
bayu-ladom-ipok <50278878+bayu-ladom-ipok@users.noreply.github.com>
2020-11-18 16:14:18 +0800

    cleaning the code
    cleaning the code

--- 5folds/train_stage01/fold5/weights.py
+++ 5folds/train_stage01/fold5/weights.py
@@ -26,31 +26,31 @@ def get_class_weights(total_counts, class_positive_counts, multiply):
     class_weights = []
     for i, class_name in enumerate(class_names):
         if(str(class_name) == 'Hernia' or str(class_name) == 'hernia'): #1
-            class_weights.append({1:0.693871743465443,0:0.306128256534557})
+            class_weights.append({1:0.6920910642865011,0:0.30790893571349887})
         if(str(class_name) == 'Pneumonia' or str(class_name) == 'pneumonia'): #2
-            class_weights.append({0:0.8838917411920432,1:0.1161082588079568})
+            class_weights.append({0:0.8839769990285963,1:0.1160230009714037})
         if(str(class_name) == 'Fibrosis' or str(class_name) == 'fibrosis'): #3
-            class_weights.append({0:0.9061002061474714,1:0.09389979385252857})
+            class_weights.append({0:0.9061170029778507,1:0.09388299702214935})
         if(str(class_name) == 'Edema' or str(class_name) == 'edema'): #4
-            class_weights.append({0:0.92555073003653,1:0.07444926996347004})
+            class_weights.append({0:0.9255054421830793,1:0.07449455781692067})
         if(str(class_name) == 'Emphysema' or str(class_name) == 'emphysema'): #5
-            class_weights.append({0:0.9258178409653085,1:0.0741821590346915})
+            class_weights.append({0:0.925771677630733,1:0.07422832236926705})
         if(str(class_name) == 'Cardiomegaly' or str(class_name) == 'cardiomegaly'): #6
-            class_weights.append({0:0.9344782137596738,1:0.06552178624032626})
+            class_weights.append({0:0.9344031562586765,1:0.06559684374132341})
         if(str(class_name) == 'Pleural_Thickening' or str(class_name) == 'pleural_thickening' or str(class_name) == 'Pleural_thickening' or str(class_name) == 'pleural_Thickening'): #7
-            class_weights.append({0:0.9438346375035852,1:0.05616536249641478})
+            class_weights.append({0:0.9437268083610394,1:0.05627319163896056})
         if(str(class_name) == 'Pneumothorax' or str(class_name) == 'pneumothorax'): #8
-            class_weights.append({0:0.9565217602732179,1:0.04347823972678215})
+            class_weights.append({0:0.9563644843607066,1:0.04363551563929341})
         if(str(class_name) == 'Consolidation' or str(class_name) == 'consolidation'): #9
-            class_weights.append({0:0.9587876429986858,1:0.041212357001314204})
+            class_weights.append({0:0.9586204385085552,1:0.04137956149144485})
         if(str(class_name) == 'Mass' or str(class_name) == 'mass'): #10
-            class_weights.append({0:0.9630991283517435,1:0.03690087164825647})
+            class_weights.append({0:0.9629114171707367,1:0.03708858282926328})
         if(str(class_name) == 'Nodule' or str(class_name) == 'nodule'): #11
-            class_weights.append({0:0.9651368188656942,1:0.034863181134305726})
+            class_weights.append({0:0.9649384039366831,1:0.03506159606331688})
         if(str(class_name) == 'Atelectasis' or str(class_name) == 'atelectasis'): #12
-            class_weights.append({0:0.9750602738068592,1:0.024939726193140763})
+            class_weights.append({0:0.9747883044112565,1:0.02521169558874344})
         if(str(class_name) == 'Effusion' or str(class_name) == 'effusion'): #13
-            class_weights.append({0:0.97635343371294,1:0.023646566287059998})
+            class_weights.append({0:0.9760659112727925,1:0.02393408872720744})
         if(str(class_name) == 'Infiltration' or str(class_name) == 'infiltration'): #4
-            class_weights.append({0:0.9792393158516902,1:0.020760684148309828})
+            class_weights.append({0.9789010181857953,1:0.021098981814204682})
     return class_weights
